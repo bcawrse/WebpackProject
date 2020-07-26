265b8ca5811f8c0f1f5416c6581d31bc6ec79953
Ben Cawrse <bcawrse@gmail.com>
2020-07-26 17:38:37 -0400

    Removed comments from react-router refactor

--- src/router.js
+++ src/router.js
@@ -3,9 +3,6 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-// import ArtistDetail from './components/artists/ArtistDetail';
-// import ArtistCreate from './components/artists/ArtistCreate';
-// import ArtistEdit from './components/artists/ArtistEdit';
 import { system } from 'faker';
 
 const componentRoutes = {
@@ -42,18 +39,5 @@ const Routes = () => {
     <Router history={hashHistory} routes={componentRoutes} />
   );
 };
-
-// const Routes = () => {
-//   return (
-//     <Router history={hashHistory}>
-//       <Route path="/" component={Home}>
-//         <IndexRoute component={ArtistMain} />
-//         <Route path="artists/new" component={ArtistCreate} />
-//         <Route path="artists/:id" component={ArtistDetail} />
-//         <Route path="artists/:id/edit" component={ArtistEdit} />
-//       </Route>
-//     </Router>
-//   );
-// };
 
 export default Routes;
